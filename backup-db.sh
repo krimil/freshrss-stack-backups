70f6b951e32b3a5094b32dd31afee9e61347cd9f
krimil <57998007+krimil@users.noreply.github.com>
2022-11-05 06:58:14 -0500

    Update backup-db.sh

--- backup-db.sh
+++ backup-db.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
 
-# cd /freshrss
+BACKUP_DIR=/backups
+
+#Databases
 source .env
 docker exec freshrss-db /bin/bash \
   -c "export PGPASSWORD=$FRESHRSS_DB_PASS \
   && pg_dump -U $FRESHRSS_DB_USER $FRESHRSS_DB_NAME" \
-  | gzip -9 > /backups/freshrss_$(date "+%F-%H%M%S").sql.gzip
+  | gzip -9 > /backups/freshrss_db_$(date "+%F-%H%M%S").sql.gzip
   
 # docker exec invidious-db /bin/bash \
 #   -c "export PGPASSWORD=$INVIDIOUS_DB_PASS \
 #   && pg_dump -U $INVIDIOUS_DB_USER $INVIDIOUS_DB_NAME" \
-#   | gzip -9 > /backups/invidious_$(date "+%F-%H%M%S").sql.gzip  
+#   | gzip -9 > /backups/invidious_db_$(date "+%F-%H%M%S").sql.gzip  
 
-# cd /
 
-BACKUP_DIR=/backups
+#Volumes
+docker run --rm --volumes-from freshrss -v $(pwd):/backup ubuntu tar zcvf /backup/freshrss_config_$(date "+%F-%H%M%S").tar.gz /config
+
+
+#Cleanup
 find $BACKUP_DIR/* -mtime +$BACKUP_DAYS -exec rm {} \;
 
+#Remote
 REMOTE=$(rclone --config /config/rclone.conf listremotes | head -n 1)
 rclone --config /config/rclone.conf sync $BACKUP_DIR $REMOTE$BACKUP_RCLONE_DEST
