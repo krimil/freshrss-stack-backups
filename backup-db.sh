3d5e56dc38a9f98e563ac58d8bd485dc62c45332
krimil <57998007+krimil@users.noreply.github.com>
2022-11-05 07:07:05 -0500

    Update backup-db.sh

--- backup-db.sh
+++ backup-db.sh
@@ -7,16 +7,16 @@ source .env
 docker exec freshrss-db /bin/bash \
   -c "export PGPASSWORD=$FRESHRSS_DB_PASS \
   && pg_dump -U $FRESHRSS_DB_USER $FRESHRSS_DB_NAME" \
-  | gzip -9 > /backups/freshrss_db_$(date "+%F-%H%M%S").sql.gzip
+  | gzip -9 > $BACKUP_DIR/freshrss_db_$(date "+%F-%H%M%S").sql.gzip
   
 # docker exec invidious-db /bin/bash \
 #   -c "export PGPASSWORD=$INVIDIOUS_DB_PASS \
 #   && pg_dump -U $INVIDIOUS_DB_USER $INVIDIOUS_DB_NAME" \
-#   | gzip -9 > /backups/invidious_db_$(date "+%F-%H%M%S").sql.gzip  
+#   | gzip -9 > $BACKUP_DIR/invidious_db_$(date "+%F-%H%M%S").sql.gzip  
 
 
 #Volumes
-docker run --rm --volumes-from freshrss -v $(pwd):/backup ubuntu tar zcvf /backup/freshrss_config_$(date "+%F-%H%M%S").tar.gz /config
+docker run --rm --volumes-from freshrss -v $(pwd):$BACKUP_DIR ubuntu tar zcvf $BACKUP_DIR/freshrss_config_$(date "+%F-%H%M%S").tar.gz /config
 
 
 #Cleanup
